a73d1d1f8cc05a839f64f10fffa489265db51aac
rai.verma <rai.verma@gmail.com>
2021-02-04 08:19:44 +0530

    4 feb

---
+++ ConsoleAppGurwinderDay1/GenericCopyClass.cs
@@ -0,0 +1,47 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace ConsoleAppGurwinderDay1
+//{
+//    class GenericCopyClass
+//    {
+//        public static List<T> CopyAtMost<T>(List<T> input, int maxElements)
+//        {
+//            int actualCount = Math.Min(input.Count, maxElements);
+//            List<T> ret = new List<T>(actualCount);
+//            for (int i = 0; i < actualCount; i++)
+//            {
+//                ret.Add(input[i]);
+//            }
+//            return ret;
+//        }
+//        static void MyFun<T,T1>(T param,T1 param2)
+//        {
+//            Console.WriteLine($"{param} is {param2} year old");
+
+//        }
+        
+//        static void Main()
+//        {
+//            Dictionary<string, int> courses = new Dictionary<string, int>
+//            {
+//                { "C", 2500 },
+//                { "C#", 5500 },
+//                { "C++", 3500 }
+//            };
+//            foreach (var course in courses)
+//            {
+//                Console.WriteLine($"Learn {course.Key} in INR{course.Value} Only.");
+//            }
+
+//            MyFun<string, int>("Ram", 23);
+//            List<int> numbers = new List<int>();
+//            numbers.Add(5);
+//            numbers.Add(10);
+//            numbers.Add(20);
+//            List<int> firstTwo = CopyAtMost<int>(numbers, 2);
+//            Console.WriteLine(firstTwo.Count);
+//        }
+//    }
+//}
---
+++ ConsoleAppGurwinderDay1/StringDemo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppGurwinderDay1
+{
+    class StringDemo
+    {
+        static void Main()
+        {
+            string name = "SIRCL";
+            Console.WriteLine("Enter Your name");
+            name=Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+            
+            Console.WriteLine($"Length of String {name.Length}");
+            string company = " SIRCL TECH Is";
+            
+            
+            if (company.Trim() == "SIRCL")
+                Console.WriteLine($"Welcome to {company}");
+            string data = "C,C++,VB,Oracle,Java";
+            
+            string[] courses = data.Split(',');
+            foreach (string course in courses)
+                Console.WriteLine(course);
+
+            if (data.Contains("VB"))
+                Console.WriteLine("VB contain in data");
+            else
+                Console.WriteLine("VB Not COntain in data");
+
+            int pos = company.IndexOf("R");
+            Console.WriteLine($"R contain at {pos} position in {company}");
+            string firstname = company.Trim().Substring(0, 5);
+            string lastname = company.Substring(6);// SIRCL TECH
+            Console.WriteLine($"{firstname.ToLower()} {lastname.ToLower()}");
+            string amount = "584";
+            Console.WriteLine($"{amount.PadLeft(6,'*').PadRight(9,'*')}");
+
+            Console.WriteLine($"Reverse order of {company} ");
+            for(int i=0;i<company.Length; i++)
+            {
+                int index = company.Length - (i + 1); 
+                Console.Write(company.Substring(index,1));
+            }
+        }
+    }
+}
