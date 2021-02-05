15e1ac1b2316c74365935281a842678943b1937c
rai.verma <rai.verma@gmail.com>
2021-02-05 08:11:13 +0530

    oop

---
+++ ConsoleAppGurwinderDay1/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppGurwinderDay1
+{
+    public class Box
+    {
+        //OOP - Technique to learn real life in the world of computer. to solve real life
+        //object - any person any place any thing 
+        //paradigms 1. Class - It has theoratical description. It does not physicaly exist in the world. It is just an Idea. 
+        //2. Object - Anything which has description and can be describe. 
+        //Relation between class and object - Class gives theoraical description about an object or it tells what type of an object is.
+        //Object - It is a real life representative of the class.
+        //3. Encapsulation - It is a process to wrap the information about an object which you want to create into a single unit. After encapsulation the
+        //encapsulated unit is known as Class. A class contain fowllowing information
+        //1. Properties - Property decide how an object look like.
+        //2. Functions- How an object work
+        //3. Events - What type of action taken by an object to perform a function.
+        //4. Event Handler - When we Associate of a function with an event then the associated function is known as event handler. 
+        //Grip is event handler of Hand and Writing is an event handler because when we want to write something we need to Grip Pen untill finish of writing 
+        //grip event handel by writing . 
+        //During encapsulation we will also decide which one member of the class should be public or private so to make a member private is known as data hiding.
+
+    }
+}
