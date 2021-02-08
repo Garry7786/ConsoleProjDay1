dcd010ad82a86249f15a3b39b35d307f56ee5a9b
rai.verma <rai.verma@gmail.com>
2021-02-08 08:12:26 +0530

    static member

---
+++ ConsoleAppGurwinderDay1/Bike.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppGurwinderDay1
+{
+    public class Bike
+    {
+        // Static Member - When we want to share a member betweenn all of the object of a class. Static member does not need an object to access .
+        //static memebr accessed by the class directly. when we any change in static property then it euquily effect all objects of the class
+        private int regno; // we want auto generate 
+        private string color;
+        private static int price=5000; // static 
+        private int mfg;
+        private static int series = 1000;
+        public Bike()
+        {
+            series = series + 1; // retain in mutiple function  
+            regno = series;
+            color = "Black";
+            mfg = DateTime.Now.Year;
+        }
+        public Bike(string color,int mfg)
+        {
+            series+=1;
+            regno = series;
+
+            this.color = color;
+            this.mfg = mfg;
+        }
+        public void display()
+        {
+            Console.WriteLine($"Regno {regno}, Bike Color {color} , Manufacturing Year {mfg} and Price {price}");
+        }
+        public static void Hike(int per) // to deal with static data member we need to create static member function
+        {
+            int hp;
+            hp = price * per / 100;
+            price = price + hp;
+        }
+        public static void Down(int per)
+        {
+            int dp;
+            dp = price * per / 100;
+            price = price - dp;
+        }
+    }
+    class BikeProgram
+    {
+        static void Main()
+        {
+            Bike bike1 = new Bike();
+            Bike bike2 = new Bike("Red",2020);
+            Bike bike3 = new Bike();
+            Bike bike4 = new Bike();
+            Bike[] bikes = new Bike[5]; // array of objects 
+
+            bike1.display();
+            bike2.display();
+            bike3.display();
+            bike4.display();
+            
+            for (int i = 0; i < 5; i++) // initialization 
+                bikes[i] = new Bike();
+
+            for (int i = 0; i < 5; i++)
+                bikes[i].display();
+
+            Bike bike = new Bike();
+            bike = bike2; // we can assign an object to another object
+
+            bike.display();
+
+            //Bike.Hike(10); // 5000*10/100=>500
+
+            //Console.WriteLine("After Hike new prices ");
+            //bike1.display(); // 5500
+            //bike2.display(); // 5500
+
+            //Bike.Down(20); // 5500*20/100 => 1100
+
+            //Console.WriteLine("After Down new prices "); //on multiple function function call static variable (price) retain itself last value
+            //bike1.display(); // 4400
+            //bike2.display(); //4400
+
+        }
+    }
+}// Student - RollNo , Name, Hindi, Eng, Math , Sci, SST , (Total, Percentage, Grade - we do not input read only) , 10 Students , Topper() , TopN(3) 
--- ConsoleAppGurwinderDay1/Programbox.cs
+++ ConsoleAppGurwinderDay1/Programbox.cs
@@ -1,46 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace ConsoleAppGurwinderDay1
-{
-    class Programbox
-    {
-        static void Main()
-        {
-            Box b1 = new Box(); // default contrctor - constructor automatically called during an object creation 
+//namespace ConsoleAppGurwinderDay1
+//{
+//    class Programbox
+//    {
+//        static void Main()
+//        {
+//            Box b1 = new Box(); // default contrctor - constructor automatically called during an object creation 
 
-            Console.WriteLine($"B1 dimension {b1.Length}x{b1.Breadth}x{b1.Height} and area is {b1.Area()} and Vol is {b1.Vol()}");
+//            Console.WriteLine($"B1 dimension {b1.Length}x{b1.Breadth}x{b1.Height} and area is {b1.Area()} and Vol is {b1.Vol()}");
 
 
-            Box b2 = new Box();
+//            Box b2 = new Box();
             
-            b1.Length = 3; // set
-            b1.Breadth = 4; // set
-            b1.Height = 2; // set
+//            b1.Length = 3; // set
+//            b1.Breadth = 4; // set
+//            b1.Height = 2; // set
 
-            Console.WriteLine("Enter Length of Box2");
-            b2.Length = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Breadth of Box2");
-            b2.Breadth = Convert.ToInt32(Console.ReadLine()); ;
-            Console.WriteLine("Enter Height of Box2");
-            b2.Height = Convert.ToInt32(Console.ReadLine()); ;
+//            Console.WriteLine("Enter Length of Box2");
+//            b2.Length = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Enter Breadth of Box2");
+//            b2.Breadth = Convert.ToInt32(Console.ReadLine()); ;
+//            Console.WriteLine("Enter Height of Box2");
+//            b2.Height = Convert.ToInt32(Console.ReadLine()); ;
 
 
-            Console.WriteLine($"B1 dimension {b1.Length}x{b1.Breadth}x{b1.Height} and area is {b1.Area()} and Vol is {b1.Vol()}");
+//            Console.WriteLine($"B1 dimension {b1.Length}x{b1.Breadth}x{b1.Height} and area is {b1.Area()} and Vol is {b1.Vol()}");
             
-            Console.WriteLine($"The Area of B2 is {b2.Area()} and Vol is {b2.Vol()}");
+//            Console.WriteLine($"The Area of B2 is {b2.Area()} and Vol is {b2.Vol()}");
 
-            Box b3 = new Box(b2);
-            Console.WriteLine($"B3 dimension {b3.Length}x{b3.Breadth}x{b3.Height} and area is {b3.Area()} and Vol is {b3.Vol()}");
+//            Box b3 = new Box(b2);
+//            Console.WriteLine($"B3 dimension {b3.Length}x{b3.Breadth}x{b3.Height} and area is {b3.Area()} and Vol is {b3.Vol()}");
             
-            /*
-             1. Create BankAccount Class - Property - AccNo, AccHolder, AccBalance - Functions CreateAcc(), ShowAccInfo(), Deposit(), WithDrawl(), ShowBalance()
-             2. Distance - Feet, Inch  Input and output 
+//            /*
+//             1. Create BankAccount Class - Property - AccNo, AccHolder, AccBalance - Functions CreateAcc(), ShowAccInfo(), Deposit(), WithDrawl(), ShowBalance()
+//             2. Distance - Feet, Inch  Input and output 
 
              
              
-             */
-        }
-    }
-}
+//             */
+//        }
+//    }
+//}
