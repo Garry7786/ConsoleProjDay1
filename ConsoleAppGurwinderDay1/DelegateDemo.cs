ba7b5c3f8b6bfb7693f0c0dbb11288ed99cd149e
rai.verma <rai.verma@gmail.com>
2021-02-17 08:17:05 +0530

    generic collection

--- ConsoleAppGurwinderDay1/DelegateDemo.cs
+++ ConsoleAppGurwinderDay1/DelegateDemo.cs
@@ -1,74 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Net;
+//using System.Text;
 
-namespace ConsoleAppGurwinderDay1
-{
-    public delegate void MyDelegate(string param); // declaration
-    public delegate int TwoIntDelegate(int param1, int param2);
+//namespace ConsoleAppGurwinderDay1
+//{
+//    public delegate void MyDelegate(string param); // declaration
+//    public delegate int TwoIntDelegate(int param1, int param2);
 
-    class DelegateDemo
-    {
-        static void Main()
-        {
-            //MyDelegate del = new MyDelegate(Hello);
-            //del("Hello World! Welcome in MyDelegate");
+//    class DelegateDemo
+//    {
+//        static void Main()
+//        {
+//            //MyDelegate del = new MyDelegate(Hello);
+//            //del("Hello World! Welcome in MyDelegate");
 
 
-            //MyDelegate msg = delegate (string val) { Console.WriteLine(val); };
-            //msg("Call of anoymous method");
+//            //MyDelegate msg = delegate (string val) { Console.WriteLine(val); };
+//            //msg("Call of anoymous method");
 
-            //PrintHelperMethod(delegate (string val) { Console.WriteLine(val); }, "Passing Delegate as Argument");
+//            //PrintHelperMethod(delegate (string val) { Console.WriteLine(val); }, "Passing Delegate as Argument");
 
-            //PrintHelperMethod(Hello, "Passing Delegate as Argument");
-            //MyDelegate del1 = new MyDelegate(MyMessage);
-            //del1("My Delegate 1");
+//            //PrintHelperMethod(Hello, "Passing Delegate as Argument");
+//            //MyDelegate del1 = new MyDelegate(MyMessage);
+//            //del1("My Delegate 1");
 
-            //TwoIntDelegate big = new TwoIntDelegate(Big);
-            //var result = big(44, 22);
-            //Console.WriteLine($"Biggest is {result}");
-            //TwoIntDelegate sum = new TwoIntDelegate(Sum);
-            //var result1 = sum(44, 22);
-            //Console.WriteLine($"Sum is {result1}");
+//            //TwoIntDelegate big = new TwoIntDelegate(Big);
+//            //var result = big(44, 22);
+//            //Console.WriteLine($"Biggest is {result}");
+//            //TwoIntDelegate sum = new TwoIntDelegate(Sum);
+//            //var result1 = sum(44, 22);
+//            //Console.WriteLine($"Sum is {result1}");
 
-            Nullable<int> i = null;
-            if (i.HasValue)
-                Console.WriteLine($"I={i}");
-            else
-                Console.WriteLine("I has null");
+//            Nullable<int> i = null;
+//            if (i.HasValue)
+//                Console.WriteLine($"I={i}");
+//            else
+//                Console.WriteLine("I has null");
             
-            Nullable<int> j = null;
-            Console.WriteLine($"j={j.GetValueOrDefault()}");
+//            Nullable<int> j = null;
+//            Console.WriteLine($"j={j.GetValueOrDefault()}");
 
-            int? k = 101;
-            int? y;
+//            int? k = 101;
+//            int? y;
 
-            y = k ?? 0; //k.HasValue ? k: 0;
-            Console.WriteLine($"Y={y}");
+//            y = k ?? 0; //k.HasValue ? k: 0;
+//            Console.WriteLine($"Y={y}");
 
-        }
-        static void PrintHelperMethod(MyDelegate mydel,string msg)
-        {
-            string newmsg = $"{msg} !  print from helper method";
-            mydel(newmsg);
+//        }
+//        static void PrintHelperMethod(MyDelegate mydel,string msg)
+//        {
+//            string newmsg = $"{msg} !  print from helper method";
+//            mydel(newmsg);
 
-        }
-        static void Hello(string msg)
-        {
-            Console.WriteLine(msg);
-        }
-        static void MyMessage(string str)
-        {
-            Console.WriteLine($"Your message is {str}");
-        }
-        static int Sum(int n1, int n2)
-        {
-            return n1 + n2;
-        }
-        static int Big(int n1, int n2)
-        {
-            return n1 > n2 ? n1 : n2;
-        }
-    }
-}
+//        }
+//        static void Hello(string msg)
+//        {
+//            Console.WriteLine(msg);
+//        }
+//        static void MyMessage(string str)
+//        {
+//            Console.WriteLine($"Your message is {str}");
+//        }
+//        static int Sum(int n1, int n2)
+//        {
+//            return n1 + n2;
+//        }
+//        static int Big(int n1, int n2)
+//        {
+//            return n1 > n2 ? n1 : n2;
+//        }
+//    }
+//}
---
+++ ConsoleAppGurwinderDay1/GenericsDemo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppGurwinderDay1
+{
+    class GenericsDemo
+    {
+        static void Main()
+        {
+            object obj;
+            obj = 10; // int => obj : boxing
+            int x;
+            x = Convert.ToInt32(obj); // unboxing
+            ArrayList al = new ArrayList();
+           
+            al.Add(10);
+            al.Add("SIRCL");
+            al.Add(3.14);
+            
+            object y = al[0];
+
+
+            List<string> courses = new List<string>() 
+            {
+                "Java",
+                "Oracle"
+            };
+            courses.Add("C");
+            courses.Add("C++");
+            foreach(var course in courses)
+            {
+                Console.WriteLine(course);
+            }
+
+            SortedList<string, int> mycourses = new SortedList<string, int>() 
+            { 
+                {"Oracle",4500},
+                {"Java",6500} 
+            };
+            
+            mycourses.Add("C", 2000);
+            mycourses.Add("C++", 2500);
+            foreach (var course in mycourses)
+            {
+                Console.WriteLine($"{course.Key} fee is {course.Value} ");
+
+            }
+            Dictionary<int, string> emplist = new Dictionary<int, string>() { {1001,"Ram" } };
+            // 
+
+            Dictionary<Dictionary<int, int>, string> grades = new Dictionary<Dictionary<int, int>, string>()
+            {
+                { new Dictionary<int, int>() { { 1000, 5000 } },"C" }
+            };
+
+            grades.Add(new Dictionary<int, int>() { { 50001, 10000 } },"B");
+
+           
+            Dictionary<int, int> newgrade = new Dictionary<int, int>();
+            newgrade.Add(10001, 15000);
+            
+            
+            grades.Add(newgrade, "A");
+
+            // CourseId , CourseName, CourseFee
+            Dictionary<int, Dictionary<string, int>> c = new Dictionary<int, Dictionary<string, int>>() {
+
+                { 1, new Dictionary<string, int>(){{ "C", 2000}}},
+                { 2, new Dictionary<string, int>(){{ "C++", 3000}}},
+                { 3, new Dictionary<string, int>(){{ "Java", 5000}}},
+                { 4, new Dictionary<string, int>(){{ "Oracle", 2000}}},
+
+
+            };
+
+            IEnumerable<string> countries; // Interface Ref
+            countries = new List<string>() {"India","Canada","US","UK"}; //  refer to list 
+            foreach(var country in countries)
+            {
+                Console.WriteLine(country);
+            }  
+
+
+        }
+    }
+}
